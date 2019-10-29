8eb6402721ed688a297783b12f8af67b3af0b27c
okFox <smayfield@gmail.com>
2019-10-29 11:02:50 -0700

    changed to list

--- src/render-image.js
+++ src/render-image.js
@@ -2,13 +2,12 @@
 
 
 export default function renderImage(card) {
-    const markup = `
-    <div class='card'>
+    const markup = `    <li class='card'>
         <div class='animal-image'><img src="${card.url}" alt="${card.title}" title="${card.title}"></div>
         <div class='animal-title'>${card.title}</div>
         <div class='horns'>${card.horns} Horn(s)</div>
         <div class='animal-description'><p>${card.description}</p></div>
-    </div>`;
+    </li>`;
     return markup;
 
 }
