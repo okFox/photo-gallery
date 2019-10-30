72e8e41e2ab323d812f9b8aadd026d16ec89f28d
okFox <smayfield@gmail.com>
2019-10-29 20:46:42 -0700

    added dynamiclly generated dropdown function and mockup image

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import renderImage from './src/render-image.js';
 import htmlToDOM from './src/html-to-dom.js';
 import images from './data/images.js';
+import renderDropList from './src/render-drop-list.js';
 
 const listElement = document.getElementById('animal-list');
-
+const dropListElement = document.getElementById('drop-list');
 
 images.forEach(object => {
     const objectHTML = renderImage(object);
     const childElement = htmlToDOM(objectHTML);
     listElement.appendChild(childElement);
 });
+
+images.forEach(object => {
+    const objectHTML = renderDropList(object);
+    const childElement = htmlToDOM(objectHTML);
+    dropListElement.appendChild(childElement);
+});
+
+
---
+++ src/render-drop-list.js
@@ -0,0 +1,6 @@
+
+export default function renderDropList(data) {
+    const markup = `<option value="${data.keyword}">${data.keyword}</option>    `;
+    return markup;
+
+}  
