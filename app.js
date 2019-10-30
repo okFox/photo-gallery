d41e09399350ef34842b1778f80441fc2499246e
okFox <smayfield@gmail.com>
2019-10-30 09:00:43 -0700

    added event listener but it needs work

--- app.js
+++ app.js
@@ -18,4 +18,26 @@ images.forEach(object => {
     dropListElement.appendChild(childElement);
 });
 
+//select may not work here.
+dropListElement.addEventListener('select', () => {
+    let selectedItemValue = dropListElement.value;
+    let filterArray = null;
 
+
+//check reference code, you need a few more steps
+    images.forEach(object => {
+        if (selectedItemValue === object.keyword) {
+            filterArray.push(object);
+            const objectHTML = renderImage(object);
+            const childElement = htmlToDOM(objectHTML);
+            listElement.appendChild(childElement);
+            
+        }
+    });
+    
+}); 
+
+
+// In app.js, change the initial render to happen in a function that is called on page load with the full list of images. Start by clearing all the elements from the list.
+// Reference the select drop-down and add an event listener
+// In the event handler, filter the images (or use all images) and call the function to re-render the list.
